42ee671f29a1f8634e3016834ed24017fb843ae5
Michal Gallus <michal.gallus@intel.com>
2018-07-03 14:06:49 +0200

    Remove unused parameter

--- fluid/ocr_recognition/crnn_ctc_model.py
+++ fluid/ocr_recognition/crnn_ctc_model.py
@@ -182,7 +182,6 @@ def ctc_train_net(images, label, args, num_classes):
     if args.average_window > 0:
         model_average = fluid.optimizer.ModelAverage(
             args.average_window,
-            params_grads,
             min_average_window=args.min_average_window,
             max_average_window=args.max_average_window)
     return sum_cost, error_evaluator, inference_program, model_average
