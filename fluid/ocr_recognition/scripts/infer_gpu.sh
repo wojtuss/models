99a14c99e1fea8b8a6341ab009b4356ab1239619
Michal Gallus <michal.gallus@intel.com>
2018-07-09 12:01:00 +0200

    Edit crnn-ctc scripts to save models to diff dirs

--- fluid/ocr_recognition/scripts/infer_gpu.sh
+++ fluid/ocr_recognition/scripts/infer_gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 OMP_NUM_THREADS=56 KMP_AFFINITY=granularity=fine,compact,1,0 python ../infer.py \
-    --model_path models/model_00001 \
+    --model_path gpu_model/model_00001 \
     --input_images_list ~/.cache/paddle/dataset/ctc_data/data/test.list \
     --input_images_dir ~/.cache/paddle/dataset/ctc_data/data/test_images \
     --use_gpu True \
--- fluid/ocr_recognition/scripts/train_cpu.sh
+++ fluid/ocr_recognition/scripts/train_cpu.sh
@@ -4,4 +4,5 @@ OMP_NUM_THREADS=56 KMP_AFFINITY=granularity=fine,compact,1,0 LD_LIBRARY_PATH=/us
     --parallel False \
     --batch_size 32 \
     --save_model_period 1 \
-    --total_step 1
+    --total_step 1 \
+    --save_model_dir cpu_model
