d9030d10f1f8fe77e138ffe10f172039237583ea
Michal Gallus <michal.gallus@intel.com>
2018-08-24 14:50:34 +0200

    CRNN-CTC: add --use_transpiler flag

--- fluid/ocr_recognition/scripts/infer_mkldnn.sh
+++ fluid/ocr_recognition/scripts/infer_mkldnn.sh
@@ -4,6 +4,7 @@ FLAGS_use_mkldnn=1 OMP_NUM_THREADS=56 KMP_AFFINITY=granularity=fine,compact,1,0 
     --input_images_list ~/.cache/paddle/dataset/ctc_data/data/test.list \
     --input_images_dir ~/.cache/paddle/dataset/ctc_data/data/test_images \
     --use_gpu False \
+    --use_transpiler True \
     --batch_size 32 \
     --iterations 5 \
     --skip_batch_num 2
