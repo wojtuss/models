092603bd6504b1efb99a3a368fe90db997c12779
Danqing Li <danqing.li@intel.com>
2018-12-03 15:02:08 +0100

    save_inference_model target vars should be list

--- fluid/PaddleCV/ocr_recognition/capi/infer_ocr_no_accuracy.sh
+++ fluid/PaddleCV/ocr_recognition/capi/infer_ocr_no_accuracy.sh
@@ -4,13 +4,13 @@ export KMP_AFFINITY=granularity=fine,compact,1,0
 export KMP_BLOCKTIME=1
 cd build
 ./infer_ocr_detection \
-  --use_mkldnn=false \
-  --data_list=/home/li \
-  --image_dir=/home/wojtuss/repos/PaddlePaddle/data/pascalvoc/ \
-  --iterations=1000  \
-	--infer_model=/home/li/models \
+  --mkldnn_used=false \
+  --data_list=/home/li/.cache/paddle/dataset/ctc_data/data/test.list \
+  --image_dir=/home/li/.cache/paddle/dataset/ctc_data/data/test_images\
+  --iterations=10  \
+	--infer_model=/home/li/AIPG-models/paddle-models/fluid/PaddleCV/ocr_recognition/models/model_00001 \
   --batch_size=1 \
-  --skip_batch_num=0 \
-	--profile \
+  --skip_batches=0 \
+	--profile=true\
 
 cd -
