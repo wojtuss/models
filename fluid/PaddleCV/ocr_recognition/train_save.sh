b08745e379ef142912cdcc2335a86178e04ea5bd
Danqing Li <danqing.li@intel.com>
2018-12-04 22:26:58 +0100

    Some readme and delete some unnecessary files

--- fluid/PaddleCV/ocr_recognition/train_save.sh
+++
@@ -1,6 +0,0 @@
-python train_save_model.py \
-    --save_model_dir=./models
-    --train_images=
-    --train_list=
-    --test_images',       str,   None,       "The directory of images to be used for test.")
-add_arg('test_list'
