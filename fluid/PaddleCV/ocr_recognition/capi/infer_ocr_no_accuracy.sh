74c621bf758fce19f2237f21b1984365fb45f111
Danqing Li <danqing.li@intel.com>
2018-12-04 01:21:16 +0100

    flags names

--- fluid/PaddleCV/ocr_recognition/capi/infer_ocr_no_accuracy.sh
+++ fluid/PaddleCV/ocr_recognition/capi/infer_ocr_no_accuracy.sh
@@ -5,13 +5,13 @@ export KMP_BLOCKTIME=1
 cd build
 ./infer_ocr_detection \
   --paddle_num_threads=14 \
-  --mkldnn_used=true \
-  --data_list=/home/sfraczek/.cache/paddle/dataset/ctc_data/data/test.list \
-  --image_dir=/home/sfraczek/.cache/paddle/dataset/ctc_data/data/test_images\
+  --use_mkldnn=true \
+  --data_list=/home/li/.cache/paddle/dataset/ctc_data/data/test.list \
+  --data_dir=/home/li/.cache/paddle/dataset/ctc_data/data/test_images\
   --iterations=100  \
-	--infer_model=/home/sfraczek/source/Paddle-models/fluid/PaddleCV/ocr_recognition/saved_models_no_acc \
-  --batch_size=50 \
-  --skip_batches=0 \
+	--infer_model=/home/li/AIPG-models/paddle-models/fluid/PaddleCV/ocr_recognition/saved_model_no_acc \
+  --batch_size=1 \
+  --skip_batch_num=0 \
 	--profile=true 
 
 cd -
