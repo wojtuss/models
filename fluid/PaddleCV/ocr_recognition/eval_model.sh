403d6d1527dce5b87ee81f370f288ab4c4d83653
Danqing Li <danqing.li@intel.com>
2018-12-04 01:23:00 +0100

    some changes to save model with acc

--- fluid/PaddleCV/ocr_recognition/eval_model.sh
+++ fluid/PaddleCV/ocr_recognition/eval_model.sh
@@ -1,11 +1,11 @@
 export KMP_AFFINITY=granularity=fine,compact,1,0
 export KMP_BLOCKTIME=1
-python eval.py \
+python infer.py \
   --model=crnn_ctc \
   --model_path=/home/li/AIPG-models/paddle-models/fluid/PaddleCV/ocr_recognition/models/model_00001 \
-  --save_model_dir=my_saved_model \
-  --input_image_list=/home/li/.cache/paddle/dataset/ctc_data/data/test.list \
-  --input_image_dir=/home/li/.cache/paddle/dataset/ctc_data/data/test_images \
+  --save_model_dir=saved_model_with_acc \
+  --input_images_list=/home/li/.cache/paddle/dataset/ctc_data/data/test.list \
+  --input_images_dir=/home/li/.cache/paddle/dataset/ctc_data/data/test_images \
   --use_gpu=False \
 
 
--- fluid/PaddleCV/ocr_recognition/infer_model.sh
+++ fluid/PaddleCV/ocr_recognition/infer_model.sh
@@ -3,7 +3,7 @@ export KMP_BLOCKTIME=1
 python infer.py \
   --model=crnn_ctc \
   --model_path=/home/li/AIPG-models/paddle-models/fluid/PaddleCV/ocr_recognition/models/model_00001 \
-  --save_model_dir=my_saved_model \
+  --save_model_dir=saved_model_no_acc \
   --input_images_list=/home/li/.cache/paddle/dataset/ctc_data/data/test.list \
   --input_images_dir=/home/li/.cache/paddle/dataset/ctc_data/data/test_images \
   --use_gpu=False \
