09cfbae819d4fda87509502effdb1d8d758bfddd
Sylwester Fraczek <sylwester.fraczek@intel.com>
2018-12-11 14:25:05 +0100

    add capi directory and unfinished app and empty data reader files

---
+++ fluid/PaddleNLP/neural_machine_translation/transformer/capi/infer_neural_machine_translation.cc
@@ -0,0 +1,99 @@
+// Copyright (c) 2018 PaddlePaddle Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//#include "paddle/fluid/inference/analysis/analyzer.h"
+#include <gflags/gflags.h>
+#include <map>
+#include <random>
+#include <string>
+#include "data_reader.h"
+#include "paddle/fluid/inference/paddle_inference_api.h"
+// Is this profiler needed, it is not provided however
+// #include "paddle/fluid/platform/profiler.h"
+#include "stats.h"
+
+DEFINE_string(infer_model, "", "Directory of the inference model.");
+DEFINE_string(all_vocab_fpath,
+              "",
+              "Path to a vocabulary file with both languages.");
+DEFINE_string(special_token,
+              "<s> <e> <unk>",
+              "The <bos>, <eos> and <unk> tokens in the dictionary.");
+DEFINE_string(test_file_pattern, "", "The pattern to match test data files.");
+DEFINE_string(token_delimiter,
+              " ",
+              "The delimiter used to split tokens in source or target "
+              "sentences. For EN-DE BPE data we provided, use spaces as token "
+              "delimiter.");
+DEFINE_int32(batch_size,
+             1,
+             "The number of examples in one run for sequence generation.");
+DEFINE_bool(use_mkldnn, false, "Use MKL-DNN.");
+DEFINE_bool(skip_passes, false, "Skip running passes.");
+DEFINE_bool(enable_graphviz,
+            false,
+            "Enable graphviz to get .dot files with data flow graphs.");
+DEFINE_bool(one_file_params,
+            false,
+            "Parameters of the model are in one file 'params' and model in a "
+            "file 'model'.");
+DEFINE_bool(profile, false, "Turn on profiler for fluid");
+DEFINE_int32(paddle_num_threads,
+             1,
+             "Number of threads for each paddle instance.");
+DEFINE_int32(beam_size, 4, "Search width for Beam Search algorithm.");
+DEFINE_int32(max_out_len,
+             255,
+             "The maximum depth(translation length) for Beam Search algorithm.")
+
+    namespace {}  // namespace
+
+namespace paddle {
+
+void PrintInfo() {
+  std::cout << std::endl
+            << "--- Used Parameters: -----------------" << std::endl
+            << "Inference model: " << FLAGS_infer_model << std::endl
+            << "Vocab file: " << FLAGS_all_vocab_fpath << std::endl
+            << "Special tokens: " << FLAGS_special_token << std::endl
+            << "Test file pattern: " << FLAGS_test_file_pattern << std::endl
+            << "Token delimiter: " << FLAGS_token_delimiter << std::endl
+            << "Batch size: " << FLAGS_batch_size << std::endl
+            << "Use MKL-DNN: " << FLAGS_use_mkldnn << std::endl
+            << "Skip passes: " << FLAGS_skip_passes << std::endl
+            << "Enable graphviz: " << FLAGS_enable_graphviz << std::endl
+            << "One file params: " << FLAGS_one_file_params << std::endl
+            << "Profile: " << FLAGS_profile << std::endl
+            << "Paddle num threads : " << FLAGS_paddle_num_threads << std::endl
+            << "Beam size : " << FLAGS_beam_size << std::endl
+            << "Max out len : " << FLAGS_max_out_len << std::endl;
+  << "--------------------------------------" << std::endl;
+}
+void Main() {
+  PrintInfo();
+  // Test variables and call everything
+}
+
+}  // namespace paddle
+
+int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  try {
+    paddle::Main();
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return 0;
+}
