c11642097e3e5407c7ffa8e792563d9121e40cf8
Danqing Li <danqing.li@intel.com>
2018-12-13 10:48:43 +0100

    for debugging

---
+++ fluid/PaddleNLP/neural_machine_translation/transformer/script/infer.sh
@@ -0,0 +1,23 @@
+export FLAGS_use_mkldnn=1
+export OMP_NUM_THREADS=1
+#export KMP_AFFINITY=granularity=fine,compact,1,0
+#export KMP_BLOCKTIME=1   
+
+#python ../infer_profile.py \
+#  --device CPU \
+#  --skip_pass_num 3 \
+#  --profile \
+#  --num_profiling_passes 5 \
+
+python -m pdb ../infer_ori.py \
+  --src_vocab_fpath ~/data/wmt16_ende_data_bpe_clean/vocab_all.bpe.32000 \
+  --trg_vocab_fpath ~/data/wmt16_ende_data_bpe_clean/vocab_all.bpe.32000 \
+  --special_token '<s>' '<e>' '<unk>' \
+  --test_file_pattern ~/data/wmt16_ende_data_bpe_clean/newstest2016.tok.bpe.32000.en-de \
+  --token_delimiter ' ' \
+  --batch_size 1 \
+  model_path ~/models/iter_100000.infer.model \
+  beam_size 4 \
+  max_out_len 255 \
+  use_gpu False
+
