ed0e873a9bfea22a132db6eab7be259c1efdd32a
Danqing Li <danqing.li@intel.com>
2018-12-19 06:08:55 +0100

    debugging script

--- fluid/PaddleNLP/neural_machine_translation/transformer/capi/infer_transformer.sh
+++ fluid/PaddleNLP/neural_machine_translation/transformer/capi/infer_transformer.sh
@@ -3,11 +3,11 @@ export KMP_BLOCKTIME=1
 export OMP_NUM_THREADS=1
 
 cd build
+#cgdb --args ./infer_neural_machine_translation \
 ./infer_neural_machine_translation \
-  --infer_model=/home/wojtuss/repos/PaddlePaddle/data/Transformer/Transformer_model \
-  --all_vocab_fpath=/home/wojtuss/repos/PaddlePaddle/data/Transformer/gen_data/wmt16_ende_data_bpe/vocab_all.bpe.32000 \
-  --test_file_pattern=/home/wojtuss/repos/PaddlePaddle/data/Transformer/gen_data/wmt16_ende_data_bpe/newstest2016.tok.bpe.32000.en-de \
-  --token_delimiter=' ' \
+  --infer_model=/home/li/models/iter_100000.infer.model \
+  --all_vocab_fpath=/home/li/data/wmt16_ende_data_bpe_clean/vocab_all.bpe.32000 \
+  --test_file_pattern=/home/li/data/wmt16_ende_data_bpe_clean/newstest2016.tok.bpe.32000.en-de \
   --batch_size=1 \
   --use_mkldnn=true \
   --skip_passes=true \
